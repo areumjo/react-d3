c9be6dde9f26076ea7c98c5b45e0333803f0e906
Areum Jo <areumjo1@gmail.com>
2020-05-01 14:14:13 -0700

    02 Line Chart, 03 Axis and scale

--- react-d3-app/src/App.js
+++ react-d3-app/src/App.js
@@ -1,29 +1,53 @@
 import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
-import { select } from "d3";
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear } from "d3";
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
-
-  // use `useRef` to allow D3 to use and manipulate <svg> defined down in return
-  const svgRef = useRef(); // create reference object
-  console.log(svgRef); // {current: undefined} it will be empty unless useEffect takes place
+  const [data, setData] = useState([75, 30, 45, 60, 20, 56, 33, 1, 30]);
+  const svgRef = useRef(); 
   
+  // will be called initially and on every `[data]` change
   useEffect(() => {
-    // console.log(svgRef); // {current: svg}
-    const svg = select(svgRef.current); // svg
+    const svg = select(svgRef.current); // generate svg element
 
-    // console.log(svg.selectAll("circle").data(data))
-    // at this point you can use any d3 method to manipulate dom or render circles
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1]) // input value
+      .range([0, 300]) // in svg width
+
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0]) // height
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat(index => index + 1);
+    const yAxis = axisRight(yScale);
+
     svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", value => value)
-      .attr("cx", value => value * 2)
-      .attr("cy", value => value * 2)
-      .attr("stroke", "red");
-  }, [data]) // a way to access <svg> dom element
+      .select(".x-axis")
+      .style("transform", "translateY(150px")
+      .call(xAxis);
+
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px")
+      .call(yAxis);
+
+    const myLine = line() // generate "d" attribute of a path element
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal)
+
+    // render path element, and attaches
+    svg
+      .selectAll(".line")
+      .data([data])
+      .join("path")
+      .attr("class", "line")
+      .attr("d", myLine)
+      .attr("fill", "none")
+      .attr("stroke", "blue");
+  }, [data]) 
 
   return (
     <div className="App">
@@ -33,11 +57,15 @@ function App() {
       <div>
         <h2>From using React <code>hooks</code> with D3</h2>
         <svg ref={svgRef}>
+          <g className="x-axis" />
+          <g className="y-axis" />
         </svg>
         {/* {data.map(v => (
             <circle r={v}></circle>
           ))} */}
         {/* </svg> */}
+        <br />
+        <br />
         <br />
         <button onClick={() => setData(data.map(value => value + 5))}>
           Update data
